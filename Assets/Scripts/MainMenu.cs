dbeb7dafddd438a56468106ae2f3708f10da1bc3
epaschke22 <ericksonpaschke@gmail.com>
2021-08-09 15:16:23 -0400

    added colorblind accessibility

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,16 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
 	{
+        if (colorblindMode.isOn)
+		{
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+		else
+		{
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene("maze");
     }
 
